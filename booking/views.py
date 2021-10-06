893449c3cbfb758be2fa2d948b9d0f5b462a168e
Ryan Hendrickson <ryanhend28@gmail.com>
2021-10-06 19:45:08 -0400

    updated commit

--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+ROOMS = [101,102,103,104]
+
 # Create your views here.
 def home(request):
     #templates folder is already assumed because this app is registered in settings.py
     name = "Ryan"
-    return render(request, 'booking/home.html', context={"user_first_name":name})
+    loggedin=False
+    context = {"user_first_name":name, "rooms":ROOMS, "loggedin":loggedin}
+    return render(request, 'booking/home.html', context=context)
 
 def about(request):
     return render(request, 'booking/about.html')
