acb5851cdbddc3ea50c767306e55e38498d00a80
Ryan Hendrickson <ryanhend28@gmail.com>
2021-10-06 23:21:26 -0400

    "week6" commit

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+BASE_PRICE = 80.00
+SUITE_UPCHARGE = 0.25
+OCEAN_UPCHARGE = 0.25
 
 # Create your models here.
+class Room(models.Model):
+    room_num = models.IntegerField(default=0)
+    price = models.FloatField(default=BASE_PRICE)
+    occupied = models.BooleanField(default=False)
+    oceanside = models.BooleanField(default=False)
+    suite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Room #" + str(self.room_num)
